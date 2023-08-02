46b88805bad61d154b38edeeaaec161f3d622265
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-02 11:08:07 +0200

    minor padding fix on beast tab

--- wonderland/components/DashboardTabs/Beasts.tsx
+++ wonderland/components/DashboardTabs/Beasts.tsx
@@ -15,13 +15,25 @@ const Beasts = () => {
 			name: 'Random',
 			image: 'https://basicbeasts.mypinata.cloud/ipfs/QmStptQqKw1aoa1U4MUF64KaxGYAx2WRoGkBuafHkLoJyD',
 		},
+		{
+			name: 'Random',
+			image: 'https://basicbeasts.mypinata.cloud/ipfs/QmStptQqKw1aoa1U4MUF64KaxGYAx2WRoGkBuafHkLoJyD',
+		},
+		{
+			name: 'Random',
+			image: 'https://basicbeasts.mypinata.cloud/ipfs/QmStptQqKw1aoa1U4MUF64KaxGYAx2WRoGkBuafHkLoJyD',
+		},
+		{
+			name: 'Random',
+			image: 'https://basicbeasts.mypinata.cloud/ipfs/QmStptQqKw1aoa1U4MUF64KaxGYAx2WRoGkBuafHkLoJyD',
+		},
 	];
 
 	const NFT = ({ item }: any) => (
 		<div className="p-0 mb-4 bg-white bg-opacity-10 border border-solid border-white border-opacity-20 rounded-xl overflow-hidden">
 			<div className="flex-auto flex flex-col w-full group relative">
 				<div className="rounded-xl overflow-hidden flex items-center relative">
-					<div className="cursor-pointer">
+					<div>
 						<Image
 							alt={item.name}
 							src={item.image}
@@ -30,7 +42,7 @@ const Beasts = () => {
 						/>
 					</div>
 					<div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-						<button className="justify-center bg-white bg-opacity-80 h-5 px-3 hover:opacity-80 flex items-center rounded-full text-sm drop-shadow text-black transition ease-in-out duration-100 group-hover:opacity-100">
+						<button className="justify-center bg-white bg-opacity-80 h-5 px-3 hover:opacity-100 flex items-center rounded-full text-sm drop-shadow text-black transition ease-in-out duration-100 group-hover:opacity-100">
 							Quest
 						</button>
 					</div>
@@ -62,7 +74,7 @@ const Beasts = () => {
 	);
 
 	return (
-		<div className="p-6">
+		<div className="pt-6">
 			<div className="grid grid-cols-4 gap-x-4 gap-y-6 overflow-auto">
 				{dummyData.map((item) => (
 					<NFT key={item.name} item={item} />

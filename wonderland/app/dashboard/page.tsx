3b8f26f7444e508fbf3e8a98c0d9dda728c1887c
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-02 09:03:19 +0200

    dashboard pt. 2

--- wonderland/app/dashboard/page.tsx
+++ wonderland/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import Image from 'next/image';
+'use client';
+
+import React, { useState } from 'react';
+
+const Tab = ({ children }: any) => <div>{children}</div>;
 
 export default function Dashboard() {
+	const [activeTab, setActiveTab] = useState('beasts');
+
 	return (
 		<div
 			className="min-h-screen flex items-center justify-center bg-center bg-no-repeat bg-cover"
@@ -14,6 +19,44 @@ export default function Dashboard() {
 				>
 					Dashboard
 				</h1>
+
+				<div className="bg-white bg-opacity-50 p-5 rounded-lg">
+					<div className="flex border-b">
+						<button
+							className={
+								activeTab === 'beasts'
+									? 'border-b-2 border-blue-500'
+									: ''
+							}
+							onClick={() => setActiveTab('beasts')}
+						>
+							Beasts
+						</button>
+						<button
+							className={
+								activeTab === 'rewards'
+									? 'border-b-2 border-blue-500'
+									: ''
+							}
+							onClick={() => setActiveTab('rewards')}
+						>
+							Rewards
+						</button>
+						<button
+							className={
+								activeTab === 'random'
+									? 'border-b-2 border-blue-500'
+									: ''
+							}
+							onClick={() => setActiveTab('random')}
+						>
+							Random
+						</button>
+					</div>
+					{activeTab === 'beasts' && <Tab>Beasts Content</Tab>}
+					{activeTab === 'rewards' && <Tab>Rewards Content</Tab>}
+					{activeTab === 'random' && <Tab>Random Content</Tab>}
+				</div>
 			</header>
 		</div>
 	);

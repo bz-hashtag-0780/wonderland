69a0bf9f278bb09c7c81cb94ad493975e24e7f1c
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-13 11:55:12 +0200

    Update RevealedModal.tsx

--- wonderland/components/ui/RevealedModal.tsx
+++ wonderland/components/ui/RevealedModal.tsx
@@ -7,7 +7,7 @@ const RevealedModal = ({ isOpen, onClose }: any) => {
 					className="absolute inset-0 bg-black opacity-50"
 					onClick={onClose}
 				></div>
-				<div className="bg-black bg-opacity-80 p-6 rounded-lg shadow-xl max-w-xl overflow-scroll z-10">
+				<div className="bg-black bg-opacity-80 p-6 rounded-lg shadow-xl w-1/3 max-w-3xl overflow-scroll z-10">
 					<div className="flex w-full justify-center items-center">
 						<h2 className="text-2xl font-bold uppercase tracking-wide">
 							Revealed
@@ -15,6 +15,22 @@ const RevealedModal = ({ isOpen, onClose }: any) => {
 					</div>
 					<div className="flex justify-center items-center border rounded-lg w-full p-4 px-6 mt-4 mb-6">
 						<div>Your rewards have been revealed!</div>
+					</div>
+					<div className="flex flex-col">
+						<div className="flex justify-between text-base mt-4">
+							<p>Sushi</p>
+							<p>1</p>
+						</div>
+						<div
+							className="flex my-4 h-px w-full"
+							style={{
+								backgroundColor: 'rgba(255, 255, 255, 0.12)',
+							}}
+						/>
+						<div className="flex justify-between text-xl">
+							<p>Total Rewards</p>
+							<p>1</p>
+						</div>
 					</div>
 
 					<button

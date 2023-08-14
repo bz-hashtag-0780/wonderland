c1d4cf4f838f5b66c228f75ed3ab8b653bc685d9
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-14 13:54:12 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,31 +11,24 @@ app.get('/', (req, res) => {
 	res.send('Hello, Express with JavaScript!');
 });
 
-app.listen(PORT, async () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
-
+async function processEligibleNFTs() {
 	const eligibleNFTs = await flowService.getRewardEligibleNFTs();
 	console.log('Eligible NFTs: ', eligibleNFTs.length);
 
 	const max = 50;
-	const keysLimit = 51;
-
-	await flowService.changeRewardPerSecond('86400.0');
-	// await flowService.changeRewardPerSecond('60.0');
-
-	await flowService.addKeys('150');
+	const keysLimit = 201;
 	const rewardPerSecond = await flowService.getRewardPerSecond();
 	console.log('Reward Per Second', rewardPerSecond);
 
-	// if (eligibleNFTs.length > 0) {
-	// 	const chunks = splitIntoChunks(eligibleNFTs, max);
-	// 	var i = 0;
-	// 	while (i < keysLimit && i < chunks.length) {
-	// 		flowService.giveRewards(chunks[i], i);
-	// 		i = i + 1;
-	// 	}
-	// }
-});
+	if (eligibleNFTs.length > 0) {
+		const chunks = splitIntoChunks(eligibleNFTs, max);
+		var i = 0;
+		while (i < keysLimit && i < chunks.length) {
+			flowService.giveRewards(chunks[i], i);
+			i = i + 1;
+		}
+	}
+}
 
 function splitIntoChunks(arr, chunkSize) {
 	const chunks = [];
@@ -44,3 +37,11 @@ function splitIntoChunks(arr, chunkSize) {
 	}
 	return chunks;
 }
+
+app.listen(PORT, async () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+	// Call the function once the server starts
+	await processEligibleNFTs();
+	// Then run the function every 12 hours
+	setInterval(processEligibleNFTs, 12 * 60 * 60 * 1000); // 12 hours in milliseconds
+});

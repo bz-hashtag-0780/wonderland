ded240ebc8dfbb645deba343224b49ccf2c13981
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-19 21:12:51 +0200

    Update RaidCockpit.tsx

--- wonderland/components/raidsContent/RaidCockpit.tsx
+++ wonderland/components/raidsContent/RaidCockpit.tsx
@@ -18,7 +18,7 @@ const RaidCockpit = ({ setOpenRaidProfile, setOpenChooseBeast }: any) => {
 		currentSeason,
 		allRecords,
 	} = useUser();
-	const { user } = useAuth();
+	const { user, loggedIn, logIn } = useAuth();
 
 	const beast =
 		raidBeast && beasts
@@ -240,178 +240,204 @@ const RaidCockpit = ({ setOpenRaidProfile, setOpenChooseBeast }: any) => {
 			<div className="flex uppercase font-bold text-3xl justify-center mb-4">
 				Raid Cockpit
 			</div>
-			{session && session.user ? (
-				<div className="flex w-full flex-col">
-					<div className="flex flex-col">
-						<div className="flex">
-							<div>
-								<div className="relative overflow-hidden w-full bg-transparent rounded-xl">
-									{beast && (
-										<Image
-											alt={'something'}
-											src={
-												'https://basicbeasts.mypinata.cloud/ipfs/' +
-												beast?.image
-											}
-											width={400}
-											height={400}
-											priority={true}
-										/>
-									)}
-								</div>
-							</div>
-							<div className="ml-4 w-full">
-								<div className="flex w-full mb-4">
-									<Info
-										value={
-											beast?.name + ' #' + beast?.serial
-										}
-										label={'Beast'}
-									/>
-									<Info
-										value={
-											beast?.sushiCount == 0 &&
-											beast?.iceCreamCount == 0
-												? 'Cannot Raid'
-												: 'Ready to Raid'
-										}
-										label={'Status'}
-										lastItem={true}
-									/>
-								</div>
-								<div className="flex w-full mb-4">
-									<Info
-										value={exp[beast?.id] || 0}
-										label={'EXP Points'}
-									/>
-									<Info
-										value={beast?.sushiCount}
-										label={'Sushi'}
-									/>
-									<Info
-										value={beast?.iceCreamCount}
-										label={'Ice Cream'}
-										lastItem={true}
-									/>
-								</div>
-								<div className="border-t border-white opacity-20 mb-1"></div>
-								<div>
-									<div className="mb-4 uppercase">
-										beast season metrics
+			{loggedIn ? (
+				<>
+					{session && session.user ? (
+						<div className="flex w-full flex-col">
+							<div className="flex flex-col">
+								<div className="flex">
+									<div>
+										<div className="relative overflow-hidden w-full bg-transparent rounded-xl">
+											{beast && (
+												<Image
+													alt={'something'}
+													src={
+														'https://basicbeasts.mypinata.cloud/ipfs/' +
+														beast?.image
+													}
+													width={400}
+													height={400}
+													priority={true}
+												/>
+											)}
+										</div>
 									</div>
-									<div className="flex w-full mb-4">
-										<Info
-											value={
-												totalBeastSeasonWins +
-												' - ' +
-												totalBeastSeasonLosses
-											}
-											label={'Win - Lose'}
-										/>
-										{points && points[currentSeason] && (
+									<div className="ml-4 w-full">
+										<div className="flex w-full mb-4">
 											<Info
 												value={
-													points[currentSeason][
-														beast?.id
-													] || 0
+													beast?.name +
+													' #' +
+													beast?.serial
 												}
-												label={'Raid Points'}
+												label={'Beast'}
+											/>
+											<Info
+												value={
+													beast?.sushiCount == 0 &&
+													beast?.iceCreamCount == 0
+														? 'Cannot Raid'
+														: 'Ready to Raid'
+												}
+												label={'Status'}
 												lastItem={true}
 											/>
-										)}
+										</div>
+										<div className="flex w-full mb-4">
+											<Info
+												value={exp[beast?.id] || 0}
+												label={'EXP Points'}
+											/>
+											<Info
+												value={beast?.sushiCount}
+												label={'Sushi'}
+											/>
+											<Info
+												value={beast?.iceCreamCount}
+												label={'Ice Cream'}
+												lastItem={true}
+											/>
+										</div>
+										<div className="border-t border-white opacity-20 mb-1"></div>
+										<div>
+											<div className="mb-4 uppercase">
+												beast season metrics
+											</div>
+											<div className="flex w-full mb-4">
+												<Info
+													value={
+														totalBeastSeasonWins +
+														' - ' +
+														totalBeastSeasonLosses
+													}
+													label={'Win - Lose'}
+												/>
+												{points &&
+													points[currentSeason] && (
+														<Info
+															value={
+																points[
+																	currentSeason
+																][beast?.id] ||
+																0
+															}
+															label={
+																'Raid Points'
+															}
+															lastItem={true}
+														/>
+													)}
+											</div>
+											<div className="flex w-full">
+												<Info
+													value={
+														totalBeastSushiWins +
+														' - ' +
+														totalBeastSushiLosses
+													}
+													label={'Sushi W - L'}
+												/>
+												<Info
+													value={
+														totalBeastIceCreamWins +
+														' - ' +
+														totalBeastIceCreamLosses
+													}
+													label={'Ice Cream W - L'}
+													lastItem={true}
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="border-t border-white opacity-20 mt-4 mb-1"></div>
+								<div>
+									<div className="mb-4 uppercase">
+										wallet season metrics
+									</div>
+									<div
+										onClick={() =>
+											console.log(session.user)
+										}
+										className="flex w-full mb-4"
+									>
+										<Info
+											value={
+												session && session.user
+													? session.user.name
+													: ''
+											}
+											label={'Discord'}
+										/>
+										<Info
+											value={
+												totalWins + ' - ' + totalLosses
+											}
+											label={'Total W - L'}
+										/>
+										<Info
+											value={totalRaids}
+											label={'Raids'}
+										/>
+										<Info
+											value={totalRaided}
+											label={'Raided'}
+											lastItem={true}
+										/>
 									</div>
 									<div className="flex w-full">
 										<Info
 											value={
-												totalBeastSushiWins +
+												totalSushiWins +
 												' - ' +
-												totalBeastSushiLosses
+												totalSushiLosses
 											}
-											label={'Sushi W - L'}
+											label={'Sushi W-L'}
 										/>
 										<Info
 											value={
-												totalBeastIceCreamWins +
+												totalIceCreamWins +
 												' - ' +
-												totalBeastIceCreamLosses
+												totalIceCreamLosses
 											}
-											label={'Ice Cream W - L'}
+											label={'Ice Cream W-L'}
+										/>
+										<Info
+											value={totalRaidPoints}
+											label={'Total Raid Points'}
 											lastItem={true}
 										/>
 									</div>
 								</div>
 							</div>
+							<div className="border-t border-white opacity-20 mt-6 mb-1"></div>
+							<div>
+								<button
+									onClick={() => setOpenRaidProfile(true)}
+									className="flex justify-center items-center mt-6 border border-solid text-white hover:bg-white hover:text-black text-2xl py-1 px-4 w-full h-16 rounded-md font-bold transition duration-150 ease-in-out"
+								>
+									Edit Raid Profile
+								</button>
+							</div>
 						</div>
-						<div className="border-t border-white opacity-20 mt-4 mb-1"></div>
+					) : (
 						<div>
-							<div className="mb-4 uppercase">
-								wallet season metrics
-							</div>
-							<div
-								onClick={() => console.log(session.user)}
-								className="flex w-full mb-4"
+							<button
+								onClick={() => signIn('discord')}
+								className="flex justify-center items-center mt-6 border border-solid text-white hover:bg-white hover:text-black text-2xl py-1 px-4 w-full h-16 rounded-md font-bold transition duration-150 ease-in-out"
 							>
-								<Info
-									value={
-										session && session.user
-											? session.user.name
-											: ''
-									}
-									label={'Discord'}
-								/>
-								<Info
-									value={totalWins + ' - ' + totalLosses}
-									label={'Total W - L'}
-								/>
-								<Info value={totalRaids} label={'Raids'} />
-								<Info
-									value={totalRaided}
-									label={'Raided'}
-									lastItem={true}
-								/>
-							</div>
-							<div className="flex w-full">
-								<Info
-									value={
-										totalSushiWins +
-										' - ' +
-										totalSushiLosses
-									}
-									label={'Sushi W-L'}
-								/>
-								<Info
-									value={
-										totalIceCreamWins +
-										' - ' +
-										totalIceCreamLosses
-									}
-									label={'Ice Cream W-L'}
-								/>
-								<Info
-									value={totalRaidPoints}
-									label={'Total Raid Points'}
-									lastItem={true}
-								/>
-							</div>
+								Connect Discord
+							</button>
 						</div>
-					</div>
-					<div className="border-t border-white opacity-20 mt-6 mb-1"></div>
-					<div>
-						<button
-							onClick={() => setOpenRaidProfile(true)}
-							className="flex justify-center items-center mt-6 border border-solid text-white hover:bg-white hover:text-black text-2xl py-1 px-4 w-full h-16 rounded-md font-bold transition duration-150 ease-in-out"
-						>
-							Edit Raid Profile
-						</button>
-					</div>
-				</div>
+					)}
+				</>
 			) : (
 				<div>
 					<button
-						onClick={() => signIn('discord')}
+						onClick={() => logIn()}
 						className="flex justify-center items-center mt-6 border border-solid text-white hover:bg-white hover:text-black text-2xl py-1 px-4 w-full h-16 rounded-md font-bold transition duration-150 ease-in-out"
 					>
-						Connect Discord
+						Connect Wallet
 					</button>
 				</div>
 			)}

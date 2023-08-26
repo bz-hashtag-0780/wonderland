a08af85d2226ae8dcdb1ac411a30857551250f62
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-08-26 03:38:00 +0200

    Update page.tsx

--- wonderland/app/new/page.tsx
+++ wonderland/app/new/page.tsx
@@ -3,20 +3,7 @@ import React from 'react';
 
 export default function New() {
 	return (
-		<div
-			className="min-h-screen flex items-center justify-center bg-center bg-no-repeat bg-cover"
-			// style={{
-			// 	backgroundImage: `url(/images/placeholders/placeholder-map.jpg)`,
-			// }}
-		>
-			{/* <header className="p-5 text-center">
-				<h1
-					className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-wonderland text-white"
-					style={{ textShadow: '2px 2px #000' }}
-				>
-					Wonderland
-				</h1>
-			</header> */}
+		<div className="min-h-screen flex items-center justify-center bg-center bg-no-repeat bg-cover">
 			<MapWithMarkers />
 		</div>
 	);

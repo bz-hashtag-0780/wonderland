b49b4a61db174768707e83dfc5d4e06e9ad0c511
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-11-10 14:18:23 +0100

    Update MusicToggle.tsx

--- wonderland/components/ui/MusicToggle.tsx
+++ wonderland/components/ui/MusicToggle.tsx
@@ -30,12 +30,45 @@ const MusicToggle = ({ audioUrl }: any) => {
 	};
 
 	return (
-		<button
-			onClick={togglePlay}
-			className="absolute bottom-100 left-0 m-4 z-10"
-		>
-			{isPlaying ? 'Stop Music' : 'Play Music'}
-		</button>
+		<div className="absolute bottom-5 left-5 m-4 z-10">
+			<button onClick={togglePlay} className="">
+				<svg
+					width="60"
+					height="60"
+					viewBox="0 0 44 44"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M6.82633 6.82633L22 0.541196L37.1737 6.82633L43.4588 22L37.1737 37.1737L22 43.4588L6.82633 37.1737L0.541196 22L6.82633 6.82633Z"
+						stroke="white"
+						stroke-opacity="1"
+						fill="white"
+						fill-opacity="0.2"
+					></path>
+					<path
+						d="M15 20V24.5H17.5L21.5 27.5V17L17.5 20H15Z"
+						stroke="white"
+						stroke-linecap="square"
+					></path>
+					{isPlaying ? (
+						<path
+							id="play"
+							d="M24 20.5C24 20.5 25 21 25 22.25C25 23.5 24 24 24 24M25 17.5C27 18.5 28 20 28 22.25C28 24.5 27 26 25 27"
+							stroke="white"
+							stroke-linecap="square"
+						></path>
+					) : (
+						<path
+							id="mute"
+							d="M27.5 20L24 24.5M24 20L27.5 24.5"
+							stroke="white"
+							stroke-linecap="square"
+						></path>
+					)}
+				</svg>
+			</button>
+		</div>
 	);
 };
 
